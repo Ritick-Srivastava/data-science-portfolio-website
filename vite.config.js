8a6deb43952fd8f587313f8814839ddd937669cb
Ritick Srivastava <riticksrivastava.2000@gmail.com>
2026-04-18 15:07:03 +0530

    Fix base URL to / for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/data-science-portfolio-website/',
+  base: '/',
   server: {
     proxy: {
       '/nim-api': {
